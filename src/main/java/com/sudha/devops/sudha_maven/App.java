e20ac02bb8d31e9da0f51de657acadfac9fedf93
devisettysudha <sudhadevisetty42@gmail.com>
2021-08-20 16:37:58 +0530

    initial check-in

---
+++ src/main/java/com/sudha/devops/sudha_maven/App.java
@@ -0,0 +1,13 @@
+package com.sudha.devops.sudha_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from sudha!" );
+    }
+}
